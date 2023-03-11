bf96e59cd8d0a14c7a79cae35fdabe25d3c0168f
Fasaclox1 <rxmufasa@gmail.com>
2023-03-11 12:11:44 +0000

    Answers

--- Questions_and_Answers/Querys.sql
+++ Questions_and_Answers/Querys.sql
@@ -28,3 +28,35 @@ FROM
     SALES AS S INNER JOIN MENU AS M ON S.PRODUCT_ID= M.PRODUCT_ID
 GROUP BY CUSTOMER_ID
 ORDER BY CUSTOMER_ID DESC;
+
+
+-- 2. How many days has each customer visited the restaurant?
+
+SELECT 
+    CUSTOMER_ID, 
+    COUNT (DISTINCT(ORDER_DATE)) AS DAYS_VISITED
+FROM 
+    SALES
+GROUP BY CUSTOMER_ID 
+
+-- 3. What was the first item from the menu purchased by each customer?
+
+SELECT 
+    CUSTOMER_ID, 
+    PRODUCT_NAME, 
+    ORDER_DATE
+FROM SALES AS SS INNER JOIN MENU AS MM ON SS.PRODUCT_ID = MM.PRODUCT_ID
+WHERE ORDER_DATE = (SELECT MIN(ORDER_DATE)
+                    FROM Sales
+                    WHERE CUSTOMER_ID = CUSTOMER_ID)
+GROUP BY CUSTOMER_ID, PRODUCT_NAME, ORDER_DATE
+ORDER BY CUSTOMER_ID
+
+--4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+SELECT
+    TOP 1 COUNT(S.PRODUCT_ID) AS TOTAL_ORDER,
+    PRODUCT_NAME
+FROM    
+    SALES AS S INNER JOIN MENU AS M ON  S.PRODUCT_ID=M.PRODUCT_ID
+GROUP BY PRODUCT_NAME
+ORDER BY TOTAL_ORDER DESC
